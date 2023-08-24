0fa7e876abb6b2a1a764c1ce327db4643c9a483b
Janio Filho <filhojanio67@gmail.com>
2023-08-23 23:14:14 -0300

    Project Created

---
+++ src/main/java/com/jan1ooo/apiclickbus/ApiClickbusApplication.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.apiclickbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClickbusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiClickbusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jan1ooo/apiclickbus/ApiClickbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan1ooo.apiclickbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClickbusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
